19d93349c257f3bcc1c2ec0202bc333bad5ed410
wxuck <152903996+wxuck@users.noreply.github.com>
2023-12-04 09:58:38 -0800

    Update Fighter.java
    Added move and started on draw

--- Entities/Fighter.java
+++ Entities/Fighter.java
@@ -1,3 +1,5 @@
+import java.awt.Graphics;
+import java.awt.Color;
 
 class Fighter extends MovingThing {
   private int speed;
@@ -7,4 +9,17 @@ class Fighter extends MovingThing {
     super(x,y,width,height);
     speed = s;
   }
+
+  public void move(String direction) {
+    if(direction.equals("LEFT")) {
+      super.setX(super.getX()-speed);
+    }
+    if(direction.equals("RIGHT")) {
+      super.setX(super.getX()+speed);
+    }
+  }
+  public void draw(Graphics window) {
+    window.setColor(Color.GREEN);
+  }
+  
 }
