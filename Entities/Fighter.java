0aa018c832358c0ec2a4c3c4a29c52321da8d3e2
wxuck <152903996+wxuck@users.noreply.github.com>
2023-12-06 08:57:43 -0800

    Added first draft of jump functionality

--- Entities/Fighter.java
+++ Entities/Fighter.java
@@ -3,7 +3,12 @@ import java.awt.Color;
 
 class Fighter extends MovingThing {
   private int speed;
-  private int health = 100;  
+  private int health = 100; 
+  
+  private final int groundY = 300;
+  private float yVelocity = 0;
+  private boolean isOnGround = true;
+  private float gravity = 0.5f;
   
   public Fighter(int x, int y, int width, int height, int s) {
     super(x,y,width,height);
@@ -11,16 +16,31 @@ class Fighter extends MovingThing {
   }
 
   public void move(String direction) {
-    if(direction.equals("LEFT")) {
+    if(direction.equals("LEFT")) 
       super.setX(super.getX()-speed);
-    }
-    if(direction.equals("RIGHT")) {
+    
+    else if(direction.equals("RIGHT")) 
       super.setX(super.getX()+speed);
+    
+    else if( direction.equals("UP") && isOnGround ) {
+      yVelocity = 2.0f;
+      isOnGround = false;
     }
+      
   }
-  public void draw(Graphics window) {
+  public void moveAndDraw(Graphics window) {
     window.setColor(Color.GREEN);
     window.fillRect(super.getX(), super.getY(), super.getWidth(), super.getHeight());
+
+    if(!isOnGround) {
+      yVelocity-=gravity;
+      super.setX( super.getX() + yVelocity );
+      if(super.getY() >= groundY()) {
+        isOnGround = true;
+        super.setY(groundY());
+        yVelocity = 0;
+      }
+    }
   }
   
 }
