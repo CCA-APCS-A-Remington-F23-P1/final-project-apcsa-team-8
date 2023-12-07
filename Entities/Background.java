a2c34b0cd240a9e6374c7b75370668b2bd5aee45
KatrielL8476 <152903895+KatrielL8476@users.noreply.github.com>
2023-12-07 08:47:03 -0800

    Create Background.java

---
+++ Entities/Background.java
@@ -0,0 +1,15 @@
+public class Background{
+
+  private final int GROUND_Y;
+  private image i;
+
+public Background(int g, image i){
+  GROUND_Y=g;
+  this.i=i;
+}
+
+  public int getGroundY(){
+    return GROUND_Y;
+  }
+
+}
