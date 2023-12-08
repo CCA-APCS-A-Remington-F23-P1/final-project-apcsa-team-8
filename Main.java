d30390ee441a1777afecfdd477d43b1befff3ffd
KatrielL8476 <152903895+KatrielL8476@users.noreply.github.com>
2023-12-08 09:28:14 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+import java.io.File;
 public class Main{
   
     public static void main(String[] args){
-      
+      File file = new File("data.txt");
+      Scanner s1 = new Scanner(file);
     }
   
 }
