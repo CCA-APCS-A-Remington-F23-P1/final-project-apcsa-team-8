789800baf41e8c3c1bd6549829df98a44c31932c
KatrielL8476 <152903895+KatrielL8476@users.noreply.github.com>
2023-12-08 09:32:58 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main{
     public static void main(String[] args){
       File file = new File("data.txt");
       Scanner s1 = new Scanner(file);
+
+      PlayGame p1 = new PlayGame(new Fighter(50,500,50,100,3,s1.nextLine()),new Fighter(700,500,50,100,3,s1.nextLine()));
     }
   
 }
