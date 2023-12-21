bc3bdfda8c707cd97c3410e9c0c88c6d3111a963
KatrielL8476 <152903895+KatrielL8476@users.noreply.github.com>
2023-12-21 08:43:12 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 import java.util.Scanner;
 import java.io.File;
 public class Main{
-  
+
     public static void main(String[] args){
       File file = new File("data.txt");
-      Scanner s1 = new Scanner(file);
+      try {
+        Scanner s1 = new Scanner(file);
+        Fighter ff1=new Fighter(50,300,120,120,3,s1.nextLine());
+        Fighter ff2 = new Fighter(600,300,120,120,3,s1.nextLine());
+        String ss=s1.next();
 
-      PlayGame p1 = new PlayGame(new Fighter(50,500,50,100,3,s1.nextLine()),new Fighter(700,500,50,100,3,s1.nextLine()));
+        Initializer i1 = new Initializer(
+                ff1,ff2,ss);
+      }
+      catch(Exception e){
+        System.out.println(e);
+      }
+    
     }
-  
+
 }
